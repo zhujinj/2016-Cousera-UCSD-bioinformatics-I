30675032f35778e3570dd8cc20c6067d37777ef1
Jinjin <zhujinj@indiana.edu>
2016-10-09 15:40:19 -0400

    Update symbol array.py

--- symbol array.py
+++ symbol array.py
@@ -1,6 +1,7 @@
 ## keep track of the total number of occurrences of C that we encounter in each window of ExtendedGenome by using a symbol array
 ## The i-th element of the symbol array is equal to the number of occurrences of the symbol in the window of length len(Genome)//2 starting at position i of ExtendedGenome.
-
+''' https://stepik.org/lesson/Peculiar-Statistics-of-the-Forward-and-Reverse-Half-Strands-23059/step/8?course=Where-in-the-Genome-Does-DNA-Replication-Begin-(Part-2)&unit=6791
+'''
 ### Sample Input:  AAAAGGGG   A
 
 ### Output is:{0: 4, 1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3}
