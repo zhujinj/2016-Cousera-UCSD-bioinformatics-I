8d5547b36adf58dd8737bd85b1d22fef2bc1856d
Jinjin <zhujinj@indiana.edu>
2016-10-09 15:39:56 -0400

    Update symbol array.py

--- symbol array.py
+++ symbol array.py
@@ -1,8 +1,3 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
 ## keep track of the total number of occurrences of C that we encounter in each window of ExtendedGenome by using a symbol array
 ## The i-th element of the symbol array is equal to the number of occurrences of the symbol in the window of length len(Genome)//2 starting at position i of ExtendedGenome.
 
